6bbf86e456f81cb0733c55be3d22b46cdc5fe62b
Chris Li <chris.lty07@gmail.com>
2022-11-07 17:48:26 -0500

    commit tree c46b10449a55cf23b58e616bf645c422ac9240f5

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.includes(item_name: item_name)
+    end
+
+    def give_away(dev, freebie)
+        if self.freebies.includes(freebie)
+            freebie.dev = dev
+            freebie.save
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,5 +17,9 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+Freebie.create(item_name: "CoffeeCup", value: 10, company_id: 1, dev_id: 4)
+Freebie.create(item_name: "Tshirt", value: 9, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "Sweater", value: 8, company_id: 3, dev_id: 2)
+Freebie.create(item_name: "Pen", value: 7, company_id: 4, dev_id: 1)
 
 puts "Seeding done!"
